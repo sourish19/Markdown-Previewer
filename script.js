9a81df4fd0891f6d7458e4873a742226facbf0b9
Sourish Dey <sourish19z@gmail.com>
2025-03-28 20:11:53 +0530

    Add reset button

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const userInput = document.querySelector("#userInput");
 const userOutput = document.querySelector(".output");
+const resetButton = document.querySelector("#resetButton");
 
 userInput.addEventListener("input", (key) => {
   userOutput.innerHTML = marked.parse(userInput.value); // Parse the markdown to text
+});
+
+// Reset
+resetButton.addEventListener("click", () => {
+  userInput.value = "";
+  userOutput.innerHTML = "";
 });
